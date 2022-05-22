cd5b183e2ec1c970ad7edcb4e3f27a8c7924868f
Kalsito1017 <kurabiika34@abv.bg>
2022-05-22 21:34:59 +0300

    Added Exercise 2

--- Stacks and Queues - Lab/2. Stack Sum/Program.cs
+++ Stacks and Queues - Lab/2. Stack Sum/Program.cs
@@ -1,4 +1,7 @@
 ﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace _2._Stack_Sum
 {
@@ -6,7 +9,52 @@ namespace _2._Stack_Sum
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+           string numberAsString = Console.ReadLine();
+            Stack<int> numbers = new Stack<int>();
+            string[] numbersList = numberAsString.Split(' ');  
+            foreach (string number in numbersList)
+            {
+                numbers.Push(int.Parse(number));
+            }
+            while (true)
+            {
+                string line = Console.ReadLine();
+                string[] lineParts = line.Split(' ');
+                string command = lineParts[0].ToLower();
+                if (command == "end")
+                {
+                    break;
+
+                }
+                else if(command == "add")
+                {
+                    // TODO: Add
+                    numbers.Push(int.Parse(lineParts[1]));
+                    numbers.Push(int.Parse(lineParts[2]));
+                }
+                else if (command == "remove")
+                {
+                    // TODO: Remove
+                    var numberofElementsToRemove = int.Parse(lineParts[1]);
+                    if (numberofElementsToRemove <= numbers.Count)
+                    {
+                        for (int i = 0; i < numberofElementsToRemove; i++)
+                        {
+                            numbers.Pop();
+                        }
+                    }
+                }
+            }
+            int sum = 0;
+            while (numbers.Count > 0)
+            {
+                int number = numbers.Pop();
+                sum += number;
+
+                
+            }
+            Console.WriteLine($"Sum: {sum}");
+                
         }
     }
 }
