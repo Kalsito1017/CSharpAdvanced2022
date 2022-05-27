0572080316f71d503258959732f4f47377d8756c
Kalsito1017 <kurabiika34@abv.bg>
2022-05-27 20:21:06 +0300

    Added the Exercise

--- Stacks and Queues - Lab/8. Traffic Jam/Program.cs
+++ Stacks and Queues - Lab/8. Traffic Jam/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace _8._Traffic_Jam
 {
@@ -6,7 +7,44 @@ namespace _8._Traffic_Jam
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            //3
+            //Enzo's car
+            //Jade's car
+            //Mercedes CLS
+            //Audi
+            //green
+            //BMW X5
+            //green
+            //end
+            int numberofCarsToPassOnGreenLight = int.Parse(Console.ReadLine()); 
+            Queue<string> cars = new Queue<string>();
+            double passedCars = 0;  
+            while (true)
+            {
+                var line = Console.ReadLine();
+                if (line == "end")
+                {
+                    break;
+                }
+                else if (line == "green")
+                {
+                    for (int i = 0; i < numberofCarsToPassOnGreenLight; i++)
+                    {
+                        if (cars.Count > 0)
+                        {
+                            var car = cars.Dequeue();
+                            Console.WriteLine(car + " passed!");
+                            passedCars++;
+                        }
+                        
+                    }
+                }
+                else 
+                {
+                    cars.Enqueue(line); //add car to the queue
+                }
+            }
+            Console.WriteLine(passedCars + " cars passed the crossroads.");
         }
     }
 }
