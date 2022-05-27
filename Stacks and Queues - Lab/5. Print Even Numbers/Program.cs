17ecb1cf2da90eda9d445c8a78f609d1b7f2cb20
Kalsito1017 <kurabiika34@abv.bg>
2022-05-27 20:48:05 +0300

    Added the Exercise

--- Stacks and Queues - Lab/5. Print Even Numbers/Program.cs
+++ Stacks and Queues - Lab/5. Print Even Numbers/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace _5._Print_Even_Numbers
 {
@@ -6,7 +8,32 @@ namespace _5._Print_Even_Numbers
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            //II.	Working with Queues
+            //11 13 18 95 2 112 81 46
+
+
+            int[] arr = Console.ReadLine().Split().Select(int.Parse).ToArray();
+            Queue<int> numbers = new Queue<int>(arr);
+            while (numbers.Count > 0)
+            {
+                if (numbers.Count == 1)
+                {
+                    Console.WriteLine(numbers.Dequeue());
+                }
+                else
+                {
+                    if (numbers.Peek() % 2 == 0)
+                    {
+                        Console.Write(numbers.Dequeue() + ", ");
+                    }
+                    else
+                    {
+                        numbers.Dequeue();
+                    }
+                }
+            }
+            
+            
         }
     }
 }
