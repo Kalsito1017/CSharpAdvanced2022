49b0bba205e4c27c5059c4d47483756179bb82a0
Kalsito1017 <kurabiika34@abv.bg>
2022-05-27 20:39:21 +0300

    Added the path

--- Stacks and Queues - Lab/7. Hot Potato/Program.cs
+++ Stacks and Queues - Lab/7. Hot Potato/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace _7._Hot_Potato
 {
@@ -6,7 +7,30 @@ namespace _7._Hot_Potato
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+           // Lucas Jacob Noah Logan Ethan
+            //10
+            //input
+            var firstLine = Console.ReadLine();
+            string[] names = firstLine.Split(' ');
+            int everyNth = int.Parse(Console.ReadLine());
+            Queue<string> kids = new Queue<string>();
+            foreach (var name in names)
+            {
+                kids.Enqueue(name);
+            }
+            while (kids.Count > 1)
+            {
+                for (int i = 0; i < everyNth - 1; i++)
+                {
+                    var potatoKid = kids.Dequeue();
+                    kids.Enqueue(potatoKid); 
+                }
+                var kidToRemove = kids.Dequeue();
+                Console.WriteLine("Removed " + kidToRemove);
+            }
+            Console.WriteLine("Last is " + kids.Dequeue()); 
+
         }
+
     }
 }
