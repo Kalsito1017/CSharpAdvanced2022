ad4aef101e53f6590768ba9e8659b62e21b36b81
Kalsito1017 <kurabiika34@abv.bg>
2022-05-30 21:49:00 +0300

    Added Exercise

--- Stacks and Queues - Exercise/04. Fast Food/Program.cs
+++ Stacks and Queues - Exercise/04. Fast Food/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace _04._Fast_Food
 {
@@ -6,7 +8,30 @@ namespace _04._Fast_Food
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            int foodfortheday = int.Parse(Console.ReadLine());
+            int[] input = Console.ReadLine().Split(" ").Select(int.Parse).ToArray();
+            Console.WriteLine(input.Max());
+            Queue<int> queueOfOrders = new Queue<int>(input);
+            while (queueOfOrders.Count > 0 && foodfortheday >= 0)
+            {
+                int currentOrderd = queueOfOrders.Peek();
+                //1000 - 100
+                if (foodfortheday - currentOrderd < 0)
+                {
+                    break;
+                }
+                //900
+                foodfortheday -= currentOrderd;
+                queueOfOrders.Dequeue();
+            }
+            if (queueOfOrders.Count > 0)
+            {
+                Console.WriteLine($"Orders left: {string.Join(" ", queueOfOrders)}");
+            }
+            else
+            {
+                Console.WriteLine("Orders complete");
+            }
         }
     }
 }
