998fa4e24f30110dc1e9205f4f03f7f76fdfd37e
Kalsito1017 <kurabiika34@abv.bg>
2022-05-30 21:11:42 +0300

    Added Exercise

--- Stacks and Queues - Exercise/01. Basic Stack Operations/Program.cs
+++ Stacks and Queues - Exercise/01. Basic Stack Operations/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace _01._Basic_Stack_Operations
 {
@@ -6,7 +8,39 @@ namespace _01._Basic_Stack_Operations
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            //5 2 13
+           // 1 13 45 32 4
+
+            
+            int[] inputNumbers = Console.ReadLine().Split(" ").Select(int.Parse).ToArray();
+            int[] numbersofElements = Console.ReadLine().Split(" ").Select(int.Parse).ToArray();
+            Stack<int> stack = new Stack<int>();
+            int toPush = inputNumbers[0];
+            int toPop = inputNumbers[1];
+            int specialNumber = inputNumbers[2];
+            for (int i = 0; i < toPush; i++)
+            {
+                stack.Push(numbersofElements[i]);
+            }
+            for (int i = 0; i < toPop; i++)
+            {
+                stack.Pop();
+            }
+            if (stack.Contains(specialNumber))
+            {
+                Console.WriteLine("true");
+            }
+            else if (stack.Count > 0)
+            {
+                Console.WriteLine(stack.Min());
+            }
+            else
+            {
+                Console.WriteLine(0);
+            }
+
+
+
         }
     }
 }
