03c73791050db9f63b13f76969c1620eab2c6b79
Kalsito1017 <kurabiika34@abv.bg>
2022-06-01 21:36:05 +0300

    Added Exercise

--- Stacks and Queues - Exercise/08. Balanced Parenthesis/Program.cs
+++ Stacks and Queues - Exercise/08. Balanced Parenthesis/Program.cs
@@ -1,4 +1,7 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Collections.Specialized;
+using System.Linq;
 
 namespace _08._Balanced_Parenthesis
 {
@@ -6,7 +9,49 @@ namespace _08._Balanced_Parenthesis
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            //{[()]}
+           string input = Console.ReadLine();
+           Stack<char> stack = new Stack<char>();
+            bool isBalanced = true;
+            
+            foreach (char item in input)
+            {
+                if (item == '{' || item == '[' || item == '(')
+                {
+                    stack.Push(item);
+                    continue; 
+                }
+                if (stack.Count == 0)
+                {
+                    isBalanced = false;
+                    break;
+                }
+                if (item == '}' && stack.Peek() == '{')
+                {
+                    stack.Pop();
+                }
+                 else if (item == ']' && stack.Peek() == '[')
+                {
+                    stack.Pop();
+                }
+                 else if (item == ')' && stack.Peek() == '(')
+                {
+                    stack.Pop();
+                }
+                else
+                {
+                    isBalanced = false;
+                    break;
+                }
+            }
+            if (!isBalanced || stack.Count > 0)
+            {
+                Console.WriteLine("NO");
+            }
+            else
+            {
+                Console.WriteLine("YES");
+            }
         }
     }
 }
