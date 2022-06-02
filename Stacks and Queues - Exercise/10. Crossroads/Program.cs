89e2789c647c89c6e749f9cf0bb1524c0ee89ce1
Kalsito1017 <kurabiika34@abv.bg>
2022-06-02 21:29:33 +0300

    Added Exercise

--- Stacks and Queues - Exercise/10. Crossroads/Program.cs
+++ Stacks and Queues - Exercise/10. Crossroads/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace _10._Crossroads
 {
@@ -6,7 +8,58 @@ namespace _10._Crossroads
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            int greenLight = int.Parse(Console.ReadLine());
+            int freeWindow = int.Parse(Console.ReadLine());
+            string input = Console.ReadLine();   
+            Queue<string> cars = new Queue<string>();
+            int passedcars = 0;
+            while (input != "END")
+            {
+                
+                if (input != "green")
+                {
+                    cars.Enqueue(input);
+                    input = Console.ReadLine();
+                    continue;
+                }
+                int currentGreenLight = greenLight;
+                // 5
+                while (currentGreenLight > 0 && cars.Count > 0)
+                {
+                    // Golf - 4 
+                    string currentCar = cars.Dequeue();
+                    if (currentGreenLight - currentCar.Length >= 0)
+                    {
+                        currentGreenLight -= currentCar.Length;
+                        passedcars++;
+                        continue;
+                    }
+                    if (currentGreenLight + freeWindow - currentCar.Length >= 0)
+                    {
+                        currentGreenLight = 0;
+                        passedcars++;
+                        continue;
+                    }
+                    //Hummer
+                    //1 + 3 = 4
+                     int hitterChar = currentGreenLight + freeWindow;
+
+
+                    Console.WriteLine("A crash happened!");
+                    Console.WriteLine($"{currentCar} was hit at {currentCar[hitterChar]}.");
+                    Environment.Exit(0);
+
+                }
+                input = Console.ReadLine();
+                //car pass - no time left
+                // car pass - time left
+                // car pass - time left + free window
+                //crash
+            }
+            Console.WriteLine("Everyone is safe.");
+            Console.WriteLine($"{passedcars} total cars passed the crossroads.");
+
+
         }
     }
 }
