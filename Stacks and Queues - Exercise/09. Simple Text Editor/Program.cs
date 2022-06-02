68f91cebf6afe0c5dc74706ff50a965a934dc389
Kalsito1017 <kurabiika34@abv.bg>
2022-06-02 20:31:05 +0300

    Added Exercise 9

--- Stacks and Queues - Exercise/09. Simple Text Editor/Program.cs
+++ Stacks and Queues - Exercise/09. Simple Text Editor/Program.cs
@@ -1,4 +1,8 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Collections.Specialized;
+using System.Linq;
+using System.Text;
 
 namespace _09._Simple_Text_Editor
 {
@@ -6,7 +10,38 @@ namespace _09._Simple_Text_Editor
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            int n = int.Parse(Console.ReadLine());
+
+            StringBuilder sb = new StringBuilder();
+            Stack<string> stack = new Stack<string>();
+            for (int i = 0; i < n; i++)
+            {
+                string[] input = Console.ReadLine().Split(' ', StringSplitOptions.RemoveEmptyEntries);
+
+                if (input[0] == "1")
+                {
+                    string currText = sb.ToString();
+                    stack.Push(currText);
+                    sb.Append(input[1]);
+                }
+                else if (input[0] == "2")
+                {
+                    string currText = sb.ToString();
+                    int symbsToRemove = int.Parse(input[1]);
+                    stack.Push(currText);
+                    sb.Remove(sb.Length - symbsToRemove, symbsToRemove);
+                }
+                else if (input[0] == "3")
+                {
+                    string text = sb.ToString();
+                    Console.WriteLine(text.ElementAt(int.Parse(input[1]) - 1));
+                }
+                else if (input[0] == "4")
+                {
+                    sb.Clear();
+                    sb.Append(stack.Pop());
+                }
+            }
         }
     }
 }
