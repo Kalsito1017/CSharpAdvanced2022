d014b0943c357545b75e0d30012bdad3239c5a5c
Kalsito1017 <kurabiika34@abv.bg>
2022-06-08 22:02:49 +0300

    Added Exercise 6 to 20/100 on the judge system and empty folder 7

---
+++ Multidimensional Arrays - Lab/6. Jagged-Array Modification/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Linq;
+
+namespace _6._Jagged_Array_Modification
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int nxn = int.Parse(Console.ReadLine());
+            int[][] jagged = ReadJaggedArray(nxn);
+            string command = Console.ReadLine();
+            while (command != "END")
+            {
+                
+                string[] vs = command.Split(' ',StringSplitOptions.RemoveEmptyEntries);
+                string comand = vs[0];
+                int row = int.Parse(vs[1]);
+                int col = int.Parse(vs[2]);
+                int value = int.Parse(vs[3]);
+
+                if (row < 0 || col < 0 || row >= jagged.Length || col >= jagged[row].Length)
+                {
+                    Console.WriteLine("Invalid coordinates");
+                }
+                else if (comand == "Add")
+                {
+                    jagged[row][col] += value;
+                }
+                else if (comand == "Subtract")
+                {
+                    jagged[row][col] -= value;
+                }
+                command = Console.ReadLine();
+
+            }
+            PrintJaggedArray(jagged);
+
+        }
+        public static int[][] ReadJaggedArray(int rows)
+        {
+            int[][] jagged = new int[rows][];
+            for (int row = 0; row < rows; row++)
+            {
+                jagged[row] = Console.ReadLine().Split().Select(int.Parse).ToArray();
+            }
+
+            return jagged;
+        }
+        public static void PrintJaggedArray(int[][] jagged)
+        {
+            foreach (var t in jagged)
+                Console.WriteLine(string.Join(" ", t));
+        }
+    }
+}
