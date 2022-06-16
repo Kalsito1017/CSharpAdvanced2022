b6777c8aa912c3d058eb429401bc57e317ca8018
Kalsito1017 <kurabiika34@abv.bg>
2022-06-16 21:39:00 +0300

    Added new folder with more exercises + exercise 1

---
+++ Multidimensional Arrays - Exercise/1. Diagonal Difference/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Linq;
+
+namespace _1._Diagonal_Difference
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int nxn = int.Parse(Console.ReadLine());
+            long[][] matrix = new long[nxn][];
+            long leftDiagonalSum = 0;
+            long rightDiagonalSum = 0;
+            for (int i = 0; i < nxn; i++)
+            {
+                matrix[i] = Console.ReadLine().Split(new[] { ' ' }, StringSplitOptions.RemoveEmptyEntries)
+                                    .Select(long.Parse)
+                                    .ToArray();
+                leftDiagonalSum += matrix[i][i];
+                rightDiagonalSum += matrix[i][matrix[i].Length - 1 - i];
+            }
+            Console.WriteLine(Math.Abs(leftDiagonalSum - rightDiagonalSum));
+
+            
+        }
+    }
+}
