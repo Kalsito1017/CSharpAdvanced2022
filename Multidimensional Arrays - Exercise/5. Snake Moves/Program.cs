1484db325ee382e0aecf52a39aa57bf2d06f837a
Kalsito1017 <kurabiika34@abv.bg>
2022-06-20 20:30:05 +0300

    Added Exercise to 100/100

--- Multidimensional Arrays - Exercise/5. Snake Moves/Program.cs
+++ Multidimensional Arrays - Exercise/5. Snake Moves/Program.cs
@@ -1,10 +1,49 @@
-﻿namespace _5._Snake_Moves
+﻿namespace _5._Snake_Moves;
+
+internal class Program
 {
-    internal class Program
+    static void Main(string[] args)
     {
-        static void Main(string[] args)
+        int[] size = Console.ReadLine()
+                   .Split(' ', StringSplitOptions.RemoveEmptyEntries)
+                   .Select(int.Parse)
+                   .ToArray();
+
+        int rows = size[0];
+        int cols = size[1];
+
+        char[,] matrix = new char[rows, cols];
+        char[] snakesLenght = Console.ReadLine().ToCharArray();
+
+        Queue<char> snake = new Queue<char>(snakesLenght);
+
+        for (int row = 0; row < rows; row++)
         {
-            Console.WriteLine("Hello, World!");
+            if (row % 2 == 0)
+            {
+                for (int col = 0; col < cols; col++)
+                {
+                    matrix[row, col] = snake.Peek();
+                    snake.Enqueue(snake.Dequeue());
+                }
+            }
+            else
+            {
+                for (int col = cols - 1; col >= 0; col--)
+                {
+                    matrix[row, col] = snake.Peek();
+                    snake.Enqueue(snake.Dequeue());
+                }
+            }
+        }
+        for (int row = 0; row < matrix.GetLength(0); row++)
+        {
+            for (int col = 0; col < matrix.GetLength(1); col++)
+            {
+                Console.Write(matrix[row, col]);
+            }
+
+            Console.WriteLine();
         }
     }
 }
