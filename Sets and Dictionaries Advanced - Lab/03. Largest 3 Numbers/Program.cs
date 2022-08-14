2a7e3a5ce1515fdfbac22a8f547a4afc8f4f9b04
Kalsito1017 <kurabiika34@abv.bg>
2022-08-14 22:19:52 +0300

    Added Exercise 3

---
+++ Sets and Dictionaries Advanced - Lab/03. Largest 3 Numbers/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Linq;
+
+namespace _03._Largest_3_Numbers
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] arr = Console.ReadLine().Split(" ").Select(int.Parse).OrderByDescending(n => n).ToArray();
+            
+            int counter = 0;
+            counter = arr.Length < 3
+                ? arr.Length : 3;
+            Console.WriteLine(string.Join(" ", arr.Take(counter)));
+        }
+    }
+}
