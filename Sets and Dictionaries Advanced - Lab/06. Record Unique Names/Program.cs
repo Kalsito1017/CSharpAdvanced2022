bffbfc70c6a24615c731d8606505bd0061bda303
Kalsito1017 <kurabiika34@abv.bg>
2022-08-16 20:35:41 +0300

    Added Exercise

---
+++ Sets and Dictionaries Advanced - Lab/06. Record Unique Names/Program.cs
@@ -0,0 +1,26 @@
+﻿
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System;
+
+namespace _06._Record_Unique_Names
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+           HashSet<string> uniqueNames = new HashSet<string>();
+            int line = int.Parse(Console.ReadLine());
+            for (int i = 0; i < line; i++)
+            {
+                string names = Console.ReadLine();
+                uniqueNames.Add(names);
+            }
+            foreach (var name in uniqueNames)
+            {
+                Console.WriteLine(name);
+            }
+        }
+    }
+}
--- Sets and Dictionaries Advanced - Lab/Sets and Dictionaries Advanced - Lab/Program.cs
+++ Sets and Dictionaries Advanced - Lab/Sets and Dictionaries Advanced - Lab/Program.cs
@@ -8,15 +8,26 @@ namespace Sets_and_Dictionaries_Advanced___Lab
     {
         static void Main(string[] args)
         {
-            //random things
-            //HashSet<string> set = new HashSet<string>();
-            var dic = new Dictionary<string, int>();
-            dic.Add("hehehah", 150);
-            if (dic.ContainsValue(150))
+            ////random things
+            ////HashSet<string> set = new HashSet<string>();
+            //var dic = new Dictionary<string, int>();
+            //dic.Add("hehehah", 150);
+            //if (dic.ContainsValue(150))
+            //{
+            //    Console.WriteLine(dic.ContainsValue(150));
+            //}
+            //Vector3 v = new Vector3();
+            HashSet<string> set = new HashSet<string>();
+            //Niki
+            set.Add("Niki");
+            set.Add("Kalsito1017");
+           if (set.Contains("Niki"))
             {
-                Console.WriteLine(dic.ContainsValue(150));
+                
             }
-            Vector3 v = new Vector3();
+            set.Remove("Niki");
+            set.EnsureCapacity(3);
+            //SortedSet<int> set2 = new SortedSet<int>();
             
                 
         }
