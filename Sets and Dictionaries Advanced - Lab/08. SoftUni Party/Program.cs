041e10a18546716b1bf81108063b2dc9ad4141d8
Kalsito1017 <kurabiika34@abv.bg>
2022-08-17 20:18:36 +0300

    Added Exercise

---
+++ Sets and Dictionaries Advanced - Lab/08. SoftUni Party/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text.RegularExpressions;
+
+namespace _08._SoftUni_Party
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            HashSet<string> guestList = new HashSet<string>();
+
+            string cmdSheets;
+            while ((cmdSheets = Console.ReadLine()) != "PARTY")
+            {
+                string guestListNumber = cmdSheets;
+
+                if (guestListNumber.Length == 8)
+                {
+                    guestList.Add(guestListNumber);
+                }
+            }
+
+            string cmdParty;
+            while ((cmdParty = Console.ReadLine()) != "END")
+            {
+                string guestNumber = cmdParty;
+                if (guestList.Contains(guestNumber))
+                {
+                    guestList.Remove(guestNumber);
+                }
+            }
+
+            Console.WriteLine(guestList.Count);
+
+            foreach (string guest in guestList.Where(g => char.IsDigit(g[0])))
+            {
+                Console.WriteLine(guest);
+            }
+            foreach (string guest in guestList.Where(g => !char.IsDigit(g[0])))
+            {
+                Console.WriteLine(guest);
+            }
+
+        }
+    }
+}
