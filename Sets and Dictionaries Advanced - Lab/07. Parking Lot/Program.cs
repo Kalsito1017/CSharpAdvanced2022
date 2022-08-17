026e10d0bb16b94de60baf920258cd2895f7e98f
Kalsito1017 <kurabiika34@abv.bg>
2022-08-17 19:03:09 +0300

    Added Exercise 7

---
+++ Sets and Dictionaries Advanced - Lab/07. Parking Lot/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text.RegularExpressions;
+
+namespace _07._Parking_Lot
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var input = Console.ReadLine();
+            var parking = new HashSet<string>();
+            while (input != "END")
+            {
+                var inputParams = Regex.Split(input, ", ");
+                if (inputParams[0] == "IN")
+                {
+                    parking.Add(inputParams[1]);
+                }
+                else
+                {
+                    parking.Remove(inputParams[1]);
+                }
+                input = Console.ReadLine();
+            }
+            foreach (var item in parking)
+            {
+                Console.WriteLine(item);
+            }
+            if (parking.Count == 0)
+            {
+                Console.WriteLine("Parking Lot is Empty");
+            }
+        }
+    }
+}
