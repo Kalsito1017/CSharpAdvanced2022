841d4782d391af4e52f1033188d6d17cbb31911e
Kalsito1017 <kurabiika34@abv.bg>
2022-08-19 19:26:20 +0300

    Added Exercise for Sets and Dictionaries with Exercise 1

---
+++ Sets and Dictionaries Advanced - Exercise/01. Unique Usernames/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+namespace _01._Unique_Usernames
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            HashSet<string> set = new HashSet<string>();
+            var line = int.Parse(Console.ReadLine());
+            for (int i = 0; i < line; i++)
+            {
+                string names = Console.ReadLine();
+                set.Add(names);
+            }
+            foreach (var name in set)
+            {
+                Console.WriteLine(name);
+            }
+        }
+    }
+}
