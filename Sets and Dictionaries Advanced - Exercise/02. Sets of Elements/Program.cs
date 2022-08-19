0289a52fe214aa30170b0578c178a1557d3c60a2
Kalsito1017 <kurabiika34@abv.bg>
2022-08-19 21:08:02 +0300

    Added Exercise

---
+++ Sets and Dictionaries Advanced - Exercise/02. Sets of Elements/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+namespace _02._Sets_of_Elements
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            int[] arr = Console.ReadLine().Split(" ").Select(int.Parse).ToArray();
+            int n = arr[0];
+            int m = arr[1];
+            HashSet<int> leftset = new HashSet<int>();
+            HashSet<int> rightset = new HashSet<int>();
+            // 3 5 7
+            for (int i = 0; i < n; i++)
+            {
+                int firstnumers = int.Parse(Console.ReadLine());
+                leftset.Add(firstnumers);
+            }
+            // 2 3 5
+            for (int i = 0; i < m; i++)
+            {
+                int secondnumbers = int.Parse(Console.ReadLine());
+                rightset.Add(secondnumbers);
+            }
+            List<int> list = leftset.Intersect(rightset).ToList();
+            Console.WriteLine(string.Join(" ", list));
+        }
+    }
+}
