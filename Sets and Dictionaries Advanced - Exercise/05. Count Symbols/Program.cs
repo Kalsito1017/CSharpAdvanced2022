35a627c9ae14e97a8bfd8eb41626dd53ceac4ca5
Kalsito1017 <kurabiika34@abv.bg>
2022-08-20 21:21:26 +0300

    Added Exercise 5

---
+++ Sets and Dictionaries Advanced - Exercise/05. Count Symbols/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+namespace _05._Count_Symbols
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string text = Console.ReadLine();
+            SortedDictionary<char, int> dictionary = new SortedDictionary<char, int>();
+
+
+            foreach (char ch in text)
+            {
+                if (!dictionary.ContainsKey(ch))
+                {
+                    dictionary[ch] = 0;
+                }
+                dictionary[ch]++;
+            }
+            foreach (var (ch,time) in dictionary)
+            {
+                Console.WriteLine($"{ch}: {time} time/s");
+            }
+        }
+    }
+}
