d9060e0f6a55f9cb1021715d2bf8707eec33bfe6
Kalsito1017 <kurabiika34@abv.bg>
2022-08-20 21:45:20 +0300

    Added Exercise 6

---
+++ Sets and Dictionaries Advanced - Exercise/06. Wardrobe/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+namespace _06._Wardrobe
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int number = int.Parse(Console.ReadLine());
+           var wardrobe = new Dictionary<string, Dictionary<string, int>>();
+            for (int i = 0; i < number; i++)
+            {
+                string[] input = Console.ReadLine().Split(" -> ");
+                string color = input[0];
+                string[] clothes = input[1].Split(',');
+                if (!wardrobe.ContainsKey(color))
+                {
+                    wardrobe.Add(color, new Dictionary<string, int>());
+                }
+                foreach (var item in clothes)
+                {
+                    if (!wardrobe[color].ContainsKey(item))
+                    {
+                        wardrobe[color].Add(item, 0);
+                    }
+                    wardrobe[color][item]++;
+                }
+
+            }
+            string[] targetClothe = Console.ReadLine().Split();
+            foreach (var color in wardrobe)
+            {
+                Console.WriteLine($"{color.Key} clothes:");
+                foreach (var (clothe, value) in color.Value)
+                {
+                    if (clothe == targetClothe[1] && color.Key == targetClothe[0])
+                    {
+                        Console.WriteLine($"* {clothe} - {value} (found!)");
+                    }
+                    else
+                    {
+                        Console.WriteLine($"* {clothe} - {value}");
+                    }
+                }
+            }
+        }
+    }
+}
