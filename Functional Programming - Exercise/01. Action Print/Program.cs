e0ba33720232bed729c16a021d7915426d5791d3
Kalsito1017 <kurabiika34@abv.bg>
2022-08-26 15:03:16 +0300

    Added Exercise 1 - 5 but Exercise 5 is 40/100 on Judge system

---
+++ Functional Programming - Exercise/01. Action Print/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace _01._Action_Print
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] names = Console.ReadLine().Split(" ");
+            foreach (string name in names)
+            {
+                Console.WriteLine(name);
+            }
+        }
+    }
+}
---
+++ Functional Programming - Exercise/05. Applied Arithmetics/Program.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+namespace _05._Applied_Arithmetics
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Action<int[]> add = numberss =>
+            {
+                for (int i = 0; i < numberss.Length; i++)
+                {
+                    numberss[i] += 1;
+                }
+                
+            };
+            Action<int[]> subtract = numberss =>
+            {
+                for (int i = 0; i < numberss.Length; i++)
+                {
+                    numberss[i] -= 1;
+                }
+               
+            };
+            Action<int[]> multiply = numberss =>
+            {
+                for (int i = 0; i < numberss.Length; i++)
+                {
+                    numberss[i] *= 2;
+                }
+            };
+            Action<int[]> PrintNumbers = numbers => Console.WriteLine(String.Join(" ", numbers));
+
+
+            int[] numbers = Console.ReadLine().Split(" ").Select(int.Parse).ToArray();
+            string comand = Console.ReadLine();
+            List<int> list = new List<int>();
+            while (true)
+            {
+                comand = Console.ReadLine();
+                if (comand == "end")
+                {
+                    break;
+                }
+                
+                if (comand == "add")
+                {
+                    
+                     add(numbers);
+                    
+                }
+                else if(comand == "multiply")
+                {
+                    
+                     multiply(numbers);
+                    
+
+                }
+                else if (comand == "subtract")
+                {
+                   
+                        subtract(numbers);
+                    
+                }
+                else if (comand == "print")
+                {
+                    PrintNumbers(numbers);
+                }
+            }
+        }
+    }
+}
