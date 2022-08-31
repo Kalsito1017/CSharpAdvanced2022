569105100181949e2e0c544784a0fd337e3061d6
Kalsito1017 <kurabiika34@abv.bg>
2022-08-31 18:23:55 +0300

    Added Exercise 3

--- Defining Classes - Lab/03. Car Constructors/Program.cs
+++ Defining Classes - Lab/03. Car Constructors/Program.cs
@@ -4,9 +4,45 @@ namespace _03._Car_Constructors
 {
     class Program
     {
+        class Car
+
+        {
+            private string make;
+            private string model;
+            private int year;
+            private double fuelQuantity;
+            private double fuelConsumption;
+
+            public Car()
+            {
+
+            }
+
+            public Car(string make, string model, int year)
+            {
+                this.make = make;
+                this.model = model;
+                this.year = year;
+            }
+            public Car(string make, string model, int year, double fuelQuantity, double fuelConsumption) : this(make, model, year)
+            {
+                this.fuelQuantity = fuelQuantity;
+                this.fuelConsumption = fuelConsumption;
+            }
+        }
+
+
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            string make = Console.ReadLine();
+            string model = Console.ReadLine();
+            int year = int.Parse(Console.ReadLine());
+            double fuelQuantity = double.Parse(Console.ReadLine());
+            double fuelConsumption = double.Parse(Console.ReadLine());
+            Car firstcar = new Car();
+            Car secondCar = new Car(make, model, year);
+            Car thirdCar = new Car(make, model, year, fuelQuantity, fuelConsumption);
+
         }
     }
 }
