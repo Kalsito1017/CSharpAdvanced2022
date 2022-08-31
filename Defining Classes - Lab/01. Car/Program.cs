56f7c7348a8c4370f6e029b87c60faf1c384d5b4
Kalsito1017 <kurabiika34@abv.bg>
2022-08-31 18:05:59 +0300

    Added Exercises with Classes

---
+++ Defining Classes - Lab/01. Car/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace _01._Car
+{
+    internal class Program
+    {
+        
+        class Car
+        {
+            private  string make;
+            public string model;
+            private string year;
+            public string Make
+            {
+                get => make;
+                set => make = value;
+
+            }
+            public string Model { get => model; set => model = value; }
+            public string Year { get => year; set => year = value; }
+
+        }
+        static void Main(string[] args)
+        {
+            Car car = new Car();
+            car.Make = "VW";
+            car.Model = "Mk";
+            car.Year = "1992";
+            Console.WriteLine($"Make: {car.Make}\nModel: {car.Model}\nYear: {car.Year}");
+        }
+    }
+}
---
+++ Defining Classes - Lab/02. Car Extension/Program.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Text;
+
+namespace _02._Car_Extension
+{
+    internal class Program
+    {
+        class CarManufacturer
+        {
+
+        }
+        class Car
+        {
+            private string make;
+            private string model;
+            private int year;
+            private double fuelQuantity;
+            private double fuelConsumption;
+            public string WhoAmI()
+            {
+                StringBuilder sb = new StringBuilder();
+                sb.AppendLine($"Make: {this.Make}");
+                sb.AppendLine($"Model: {this.Model}");
+                sb.AppendLine($"Year: {this.Year}");
+                sb.AppendLine($"Fuel: {this.FuelQuantity:F2}L");
+                return sb.ToString();
+            }
+
+
+            public string Make { get => make; set => make = value; }
+            public string Model { get => model; set => model = value; }
+            public int Year { get => year; set => year = value; }
+            public double FuelQuantity { get => fuelQuantity; set => fuelQuantity = value; }
+            public double FuelConsumption { get => fuelConsumption; set => fuelConsumption = value; }
+            public void Drive(double distance)
+            {
+                var consumption = distance * (FuelConsumption / 100.0);
+                if (consumption <= FuelQuantity)
+                {
+                    fuelQuantity -= consumption;
+                }
+                else
+                {
+                    Console.WriteLine("Not enough fuel to perform this trip!");
+                }
+            }
+        }
+        static void Main(string[] args)
+        {
+            Car car = new Car();
+            car.Make = "VW";
+            car.Model = "MK3";
+            car.Year = 1992;
+            car.FuelConsumption = 200;
+            car.FuelQuantity = 200;
+            car.Drive(2000);
+            Console.WriteLine(car.WhoAmI());
+        }
+    }
+}
