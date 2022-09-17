9b9b5a02c74ac4b5556a0df2582769d809a810b3
Kalsito1017 <kurabiika34@abv.bg>
2022-09-17 20:49:17 +0300

    Added Exercise 8 and Exercise 9 to 100/100

--- Multidimensional Arrays - Exercise/8. Bombs/Program.cs
+++ Multidimensional Arrays - Exercise/8. Bombs/Program.cs
@@ -1,10 +1,113 @@
-﻿namespace _8._Bombs
+﻿
+using System;
+using System.Linq;
+namespace _8._Bombs
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            int size = int.Parse(Console.ReadLine());
+            int[,] matrix = new int[size, size];
+            for (int row = 0; row < size; row++)
+            {
+                int[] numbers = Console.ReadLine().Split().Select(int.Parse).ToArray();
+                for (int col = 0; col < size; col++)
+                {
+                    matrix[row, col] = numbers[col];
+                }
+            }
+            int[] indexes = Console.ReadLine().Split(new char[] {' ', ','}, StringSplitOptions.RemoveEmptyEntries).Select(int.Parse).ToArray();
+            for (int i = 0; i < indexes.Length; i+=2)
+            {
+                int row = indexes[i];
+                int col = indexes[i + 1];
+                if (matrix[row, col] <= 0)
+                {
+                    continue;
+                }
+                int value = matrix[row, col];
+
+                //up, down, left, right, upright, upleft,downLeft, downRight
+                //up
+                if (IsInside(matrix, row - 1,col) && matrix[row - 1, col] > 0 )
+                {
+                    matrix[row - 1, col] -= value;
+                }
+                //down
+                if (IsInside(matrix, row + 1, col) && matrix[row + 1, col] > 0)
+                {
+                    matrix[row + 1, col] -= value;
+                }
+                //left
+                if (IsInside(matrix, row , col - 1) && matrix[row , col - 1] > 0)
+                {
+                    matrix[row , col - 1] -= value;
+                }
+                //right
+                if (IsInside(matrix, row , col + 1) && matrix[row , col + 1] > 0)
+                {
+                    matrix[row , col + 1] -= value;
+                }
+                //upRight
+                if (IsInside(matrix, row - 1,col + 1) && matrix[row - 1, col + 1] > 0 )
+                {
+                    matrix[row - 1, col + 1] -= value;
+                }
+                //upLeft
+                if (IsInside(matrix, row - 1, col - 1) && matrix[row - 1, col - 1] > 0)
+                {
+                    matrix[row - 1, col - 1] -= value;
+                }
+                //downLeft
+                if (IsInside(matrix, row + 1, col + 1) && matrix[row + 1, col + 1] > 0)
+                {
+                    matrix[row + 1, col + 1] -= value;
+                }
+                //downRight
+                if (IsInside(matrix, row + 1, col - 1) && matrix[row + 1, col - 1] > 0)
+                {
+                    matrix[row + 1, col - 1] -= value;
+                }
+                matrix[row, col] = 0;
+
+            }
+            int aliveCount = 0;
+            int aliveSum = 0;
+            for (int row = 0; row < matrix.GetLength(0); row++)
+            {
+                for (int col = 0; col < matrix.GetLength(1); col++)
+                {
+                    if (matrix[row, col] > 0)
+                    {
+                        aliveCount++;
+                        aliveSum += matrix[row, col];
+                    }
+                }
+            }
+
+            Console.WriteLine($"Alive cells: {aliveCount}");
+            Console.WriteLine($"Sum: {aliveSum}");
+
+            PrintMatrix(matrix);
+
+
+        }
+        private static bool IsInside(int[,] field, int row, int col)
+        {
+            return row >= 0 && col >= 0 && field.GetLength(0) > row && field.GetLength(1) > col;
+        }
+        private static void PrintMatrix(int[,] matrix)
+        {
+            for (int row = 0; row < matrix.GetLength(0); row++)
+            {
+                for (int col = 0; col < matrix.GetLength(1); col++)
+                {
+                    Console.Write(matrix[row, col] + " ");
+                }
+
+                Console.WriteLine();
+            }
         }
     }
 }
--- Multidimensional Arrays - Exercise/9. Miner/Program.cs
+++ Multidimensional Arrays - Exercise/9. Miner/Program.cs
@@ -12,21 +12,18 @@ namespace _9._Miner
             // e the end of the route
             // c coal
             // s the place where the miner starts
-            int sizeofmatrix = int.Parse(Console.ReadLine());
-            string[] commands = Console.ReadLine().Split(" ", StringSplitOptions.RemoveEmptyEntries);
+            int sizeofmatrix = int.Parse(Console.ReadLine()!);
+            string[] commands = Console.ReadLine()?.Split(" ", StringSplitOptions.RemoveEmptyEntries);
             int collectedcoals = 0;
             char[,] field = new char[sizeofmatrix, sizeofmatrix];
             FillMatrix(field);
-            int minerRow = 0;
-            int minerCol = 0;
+            int coals = CountCoals(field);
+            int minerRow = -1;
+            int minerCol = -1;
             for (int row = 0; row < sizeofmatrix; row++)
             {
-
-
                 for (int col = 0; col < sizeofmatrix; col++)
                 {
-
-
                     if (field[row, col] == 's')
                     {
                         minerRow = row;
@@ -39,24 +36,23 @@ namespace _9._Miner
                     }
                 }
             }
-            int currCoal = 0;
             int currRow = minerRow;
             int currCol = minerCol;
 
 
             foreach (var command in commands)
             {
-                switch(command)
+                switch (command)
                 {
                     case "up":
-                        if (isInside(field, currRow - 1, currCol))
+                        if (IsInside(field, currRow - 1, currCol))
                         {
                             currRow--;
                             if (field[currRow, currCol] == 'c')
                             {
-                                currCoal++;
+                                coals--;
 
-                                if (currCoal == collectedcoals)
+                                if (coals == 0)
                                 {
                                     Console.WriteLine($"You collected all coals! ({currRow}, {currCol})");
                                     return;
@@ -73,15 +69,15 @@ namespace _9._Miner
                         }
                         break;
                     case "down":
-                        if (isInside(field, currRow + 1, currCol))
+                        if (IsInside(field, currRow + 1, currCol))
                         {
                             currRow++;
 
                             if (field[currRow, currCol] == 'c')
                             {
-                                currCoal++;
+                                coals--;
 
-                                if (currCoal == collectedcoals)
+                                if (coals == 0)
                                 {
                                     Console.WriteLine($"You collected all coals! ({currRow}, {currCol})");
                                     return;
@@ -97,15 +93,15 @@ namespace _9._Miner
                         }
                         break;
                     case "left":
-                        if (isInside(field, currRow, currCol - 1))
+                        if (IsInside(field, currRow, currCol - 1))
                         {
                             currCol--;
 
                             if (field[currRow, currCol] == 'c')
                             {
-                                currCoal++;
+                                coals--;
 
-                                if (currCoal == collectedcoals)
+                                if (coals == 0)
                                 {
                                     Console.WriteLine($"You collected all coals! ({currRow}, {currCol})");
                                     return;
@@ -121,15 +117,15 @@ namespace _9._Miner
                         }
                         break;
                     case "right":
-                        if (isInside(field, currRow, currCol + 1))
+                        if (IsInside(field, currRow, currCol + 1))
                         {
                             currCol++;
 
                             if (field[currRow, currCol] == 'c')
                             {
-                                currCoal++;
+                                coals--;
 
-                                if (currCoal == collectedcoals)
+                                if (coals == 0)
                                 {
                                     Console.WriteLine($"You collected all coals! ({currRow}, {currCol})");
                                     return;
@@ -148,24 +144,39 @@ namespace _9._Miner
             }
 
 
-            Console.WriteLine($"{collectedcoals - currCoal} coals left. ({currRow}, {currCol})");
+            Console.WriteLine($"{coals} coals left. ({currRow}, {currCol})");
         }
 
         private static void FillMatrix(char[,] field)
         {
             for (int row = 0; row < field.GetLength(0); row++)
             {
-                char[] rowItems = Console.ReadLine().Split(" ", StringSplitOptions.RemoveEmptyEntries).Select(char.Parse).ToArray();
+                char[] rowItems = Console.ReadLine()?.Split(" ", StringSplitOptions.RemoveEmptyEntries).Select(char.Parse).ToArray();
 
                 for (int col = 0; col < field.GetLength(1); col++)
                 {
-                    field[row, col] = rowItems[col];
+                    field[row, col] = rowItems![col];
                 }
             }
         }
-        private static bool isInside(char[,] field, int row, int col)
+        private static bool IsInside(char[,] field, int row, int col)
         {
             return row >= 0 && col >= 0 && field.GetLength(0) > row && field.GetLength(1) > col;
+        }
+        public static int CountCoals(char[,] matrix)
+        {
+            int count = 0;
+            for (int row = 0; row < matrix.GetLength(0); row++)
+            {
+                for (int col = 0; col < matrix.GetLength(1); col++)
+                {
+                    if (matrix[row, col] == 'c')
+                    {
+                        count++;
+                    }
+                }
+            }
+            return count;
         }
     }
     
