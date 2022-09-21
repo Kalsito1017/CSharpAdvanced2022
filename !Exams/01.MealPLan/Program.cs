8879617cf84fb5a9d0a1e51ed8dd7ad8f523ffff
Kalsito1017 <kurabiika34@abv.bg>
2022-09-21 20:50:29 +0300

    Edited name

---
+++ !Exams/01.MealPLan/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace MealPlan
+{
+    internal class MealPlan
+    {
+        static void Main(string[] args)
+        {
+            string[] mealsthatJohncaneat = Console.ReadLine().Split(" ");
+            int[] maximumcaloriesintakeperday = Console.ReadLine().Split(" ").Select(int.Parse).ToArray();
+            Stack<int> calories = new Stack<int>(maximumcaloriesintakeperday);
+            Queue<string> meals = new Queue<string>(mealsthatJohncaneat);
+            int currDailyCalories = 0;
+          
+            Dictionary<string, int> mealCalories = new Dictionary<string, int>
+            {
+                {"salad", 350},
+                {"soup", 490},
+                {"pasta", 680},
+                {"steak", 790}
+             };
+
+            while (calories.Count != 0 && meals.Count != 0)
+            {
+                int diff = currDailyCalories;
+                currDailyCalories = calories.Peek() + diff;
+                while (currDailyCalories > 0)
+                {
+                    if (meals.Count == 0)
+                    {
+                        calories.Pop();
+                        calories.Push(currDailyCalories);
+                        
+                        Console.WriteLine($"John had {mealsthatJohncaneat.Length} meals.");
+                        Console.WriteLine($"For the next few days, he can eat {string.Join(", ", calories)} calories.");
+                        return;
+                    }
+                    string currMeal = meals.Dequeue();
+                    currDailyCalories -= mealCalories[currMeal];
+                }
+                calories.Pop();
+            }
+
+
+
+            Console.WriteLine($"John ate enough, he had {mealsthatJohncaneat.Length - meals.Count} meals.");
+            Console.WriteLine($"Meals left: {string.Join(", ", meals)}.");
+
+
+
+
+        }
+    }
+}
---
+++ !Exams/ExamPreparation(C# 2022 September)/03. Stock Market/Investor.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace StockMarket
+{
+    public class Investor
+    {
+        
+        public List<Stock> Portfolio { get; set; }
+        public string Fullname { get; set; }
+        public string EmailAddress { get; set; }
+        public decimal MoneyToInvest { get; set; }
+        public string BrokerName { get; set; }
+        public Investor(string fullname, string emailAddress, decimal moneyToInvest, string brokerName)
+        {
+            Portfolio = new List<Stock>();  
+            Fullname = fullname;
+            EmailAddress = emailAddress;
+            MoneyToInvest = moneyToInvest;
+            BrokerName = brokerName;
+        }
+        public int Count => Portfolio.Count;
+        public void BuyStock(Stock stock)
+        {
+            if (stock.MarketCapitalization > 10000 && this.MoneyToInvest >= stock.PricePerShare)
+            {
+                this.Portfolio.Add(stock);
+                this.MoneyToInvest -= stock.PricePerShare;
+            }
+        }
+        public string SellStock(string companyName, decimal sellPrice)
+        {
+            if (Portfolio.Any(s => s.CompanyName == companyName) == false)
+            {
+                return $"{companyName} does not exist.";
+
+            }
+            if (sellPrice < Portfolio.First(s => s.CompanyName == companyName).PricePerShare)
+            {
+                return $"Cannot sell {companyName}.";
+            }
+            Portfolio.Remove(Portfolio.First(s => s.CompanyName == companyName));
+            this.MoneyToInvest += sellPrice;
+            return $"{companyName} was sold.";
+
+        }
+        public Stock FindStock(string companyName)
+        {
+            return Portfolio.FirstOrDefault(s => s.CompanyName == companyName);
+        }
+        public Stock FindBiggestCompany()
+        {
+            return Portfolio.OrderByDescending(s => s.MarketCapitalization).FirstOrDefault();
+
+        }
+        public string InvestorInformation()
+        {
+            StringBuilder sb = new StringBuilder();
+            sb.AppendLine($"The investor {Fullname} with a broker {BrokerName} has stocks:");
+            foreach (var stock in Portfolio)
+            {
+                sb.AppendLine(stock.ToString());
+            }
+            return sb.ToString().TrimEnd();
+        }
+    }
+}
