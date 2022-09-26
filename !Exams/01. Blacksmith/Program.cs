c0c62c664357ad4715a3437c36046fd4fc05739f
Kalsito1017 <kurabiika34@abv.bg>
2022-09-26 20:18:11 +0300

    Added 2 Exercises BlacksSmith is 100/100 and Drones are 93/100 on the judge system

---
+++ !Exams/01. Blacksmith/Program.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Xml.Linq;
+
+namespace _01._Blacksmith
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var steelInput = Console.ReadLine().Split(" ", StringSplitOptions.RemoveEmptyEntries).Select(int.Parse).ToArray();
+            var carbonInput = Console.ReadLine().Split(" ",StringSplitOptions.RemoveEmptyEntries).Select(int.Parse).ToArray();
+            var steel = new Queue<int>(steelInput);
+            var carbon = new Stack<int>(carbonInput);
+            var swords = new Dictionary<string, int[]>
+            {
+                {"Gladius", new int[2]{70,0}},
+                {"Shamshir", new int[2]{80,0}},
+                {"Katana", new int[2]{90,0}},
+                {"Sabre", new int[2]{110,0}},
+                {"Broadsword", new int[2]{150,0}},
+            };
+            int counter = 0;
+            while (steel.Count > 0 && carbon.Count > 0)
+            {
+                var currCarbon = carbon.Peek();
+                var currSteel = steel.Peek();
+                bool isMade = false;
+                foreach (var sword in swords)
+                {
+                    if (currSteel + currCarbon == sword.Value[0])
+                    {
+                        sword.Value[1]++;
+                        
+                        carbon.Pop();
+                        counter++;
+                        isMade = true;
+                        break;
+                    }
+                }
+                steel.Dequeue();
+                if (isMade)
+
+                {
+                    continue;
+                }
+                currCarbon += 5;
+                carbon.Pop();
+                carbon.Push(currCarbon);
+            }
+            Console.WriteLine(counter > 0
+                ? $"You have forged {counter} swords." 
+                 : "You did not have enough resources to forge a sword.");
+            Console.WriteLine(steel.Count > 0 ? $"Steel left: {string.Join(", ", steel)}" : "Steel left: none");
+
+            Console.WriteLine(carbon.Count > 0 ? $"Carbon left: {string.Join(", ", carbon)}" : "Carbon left: none");
+
+            foreach (var sword in swords.Where(sword => sword.Value[1] > 0).OrderBy(sword => sword.Key))
+            {
+                Console.WriteLine($"{sword.Key}: {sword.Value[1]}");
+            }
+
+
+
+        }
+    }
+}
---
+++ !Exams/03.Drones/Airfield.cs
@@ -0,0 +1,107 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+
+namespace Drones
+{
+    public class Airfield
+    {
+        public List<Drone> Drones { get; set; }
+        public string Name { get; set; }
+        public int Capacity { get; set; }
+        public double LandingStrip { get; set; }
+        public Airfield(string name, int capacity, double landingStrip)
+        {
+            Drones = new List<Drone>();
+            Name = name;
+            Capacity = capacity;
+            LandingStrip = landingStrip;
+        }
+        public int Count => Drones.Count;
+        public string AddDrone(Drone drone)
+        {
+            if (string.IsNullOrEmpty(drone.Name) || string.IsNullOrEmpty(drone.Brand) || drone.Range <= 5 || drone.Range >= 15)
+            {
+                return "Invalid drone.";
+            }
+            if (Capacity == 0)
+            {
+                return "Airfield is full.";
+            }
+
+            Drones.Add(drone);
+            Capacity--;
+
+            return $"Successfully added {drone.Name} to the airfield.";
+
+
+
+        }
+        public bool RemoveDrone(string name)
+        {
+            Drone drone = Drones.FirstOrDefault(x => x.Name == name);
+            if (drone != default)
+            {
+                Capacity++;
+                return Drones.Remove(drone);
+            }
+            return false;
+        }
+        public int RemoveDroneByBrand(string brand)
+        {
+            int count = 0;
+            for (int i = 0; i < Drones.Count; i++)
+            {
+                if (Drones[i].Brand == brand)
+                {
+                    count++;
+                    Drones.Remove(Drones[i]);
+                    i--;
+                }
+            }
+            Capacity += count;
+            return count;
+        }
+        public Drone FlyDrone(string name)
+        {
+            Drone drone = Drones.FirstOrDefault(s => s.Name == name);
+            if (drone != default)
+            {
+                drone.Available = false;
+                return drone;
+
+            }
+            else
+            {
+                return null;
+            }
+        }
+        public List<Drone> FlyDronesByRange(int range)
+        {
+            List<Drone> drones = new List<Drone>();
+            for (int i = 0; i < Drones.Count; i++)
+            {
+                if (Drones[i].Range == range)
+                {
+                    Drones[i].Available = false;
+                    drones.Add(Drones[i]);
+                }
+            }
+            return drones;
+
+        }
+        public string Report()
+        {
+            StringBuilder sb = new StringBuilder();
+            sb.AppendLine($"Drones available at {this.Name}:");
+            foreach (var drones in Drones.Where(drones => drones.Available == true))
+            {
+                sb.AppendLine(drones.ToString());
+            }
+            return sb.ToString().TrimEnd();
+        }
+
+    }
+}
