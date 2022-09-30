d626f2fe6a1a9aaade9628e2a812d35521951913
Kalsito1017 <kurabiika34@abv.bg>
2022-09-30 18:41:21 +0300

    Added Exercise 2

---
+++ Defining Classes - Exercise/02. Creating Constructors/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace DefiningClasses
+{
+    public class StartUp
+    {
+        static void Main(string[] args)
+        {
+            int count = int.Parse(Console.ReadLine());
+
+            Family family = new Family();
+
+            for (int i = 0; i < count; i++)
+            {
+                string[] input = Console.ReadLine().Split(" ", StringSplitOptions.RemoveEmptyEntries);
+
+                Person currPerson = new Person(input[0], int.Parse(input[1]));
+
+                family.AddMember(currPerson);
+            }
+
+            Person eldestPerson = family.GetOldestMember();
+
+            Console.WriteLine($"{eldestPerson.Name} {eldestPerson.Age}");
+        }
+    }
+}
+
