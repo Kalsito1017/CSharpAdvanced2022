136689ec706a112cce83837e7f0e576701f63dc5
Kalsito1017 <kurabiika34@abv.bg>
2022-10-01 13:57:54 +0300

    Added Generics Lab (i dont know why judge dont accept its path)

---
+++ Generics - Lab/01. Box/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace BoxOfT
+{
+    public class StartUp
+    {
+        static void Main(string[] args)
+        {
+            Box<int> box = new Box<int>();
+            box.Add(1);
+            box.Add(2);
+            box.Add(3);
+            Console.WriteLine(box.Remove());
+            box.Add(4);
+            box.Add(5);
+            Console.WriteLine(box.Remove());
+        }
+    }
+    public class Box <T>
+    {
+        public List<T> internalList = new List<T>();
+        public void Add(T element)
+        {
+            internalList.Add(element);
+        }
+        int Count => internalList.Count;
+        public T Remove()
+        {
+            var element = internalList[Count - 1];
+            internalList.RemoveAt(Count - 1);
+            return element;
+        }
+    }
+
+}
