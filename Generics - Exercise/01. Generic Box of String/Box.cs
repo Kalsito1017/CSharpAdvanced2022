aac08808cbc76038a505c1032e7ac492fa1edcaf
Kalsito1017 <kurabiika34@abv.bg>
2022-10-01 14:37:15 +0300

    Added Generics Exercises

---
+++ Generics - Exercise/01. Generic Box of String/Box.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Box
+{
+    public class Box<T>
+    {
+        private List<T> items;
+        public Box()
+        {
+            items = new List<T>();
+        }
+        public void Add(T item)
+        {
+            items.Add(item);
+        }
+        public override string ToString()
+        {
+            StringBuilder sb = new StringBuilder();
+            foreach (var item in items)
+            {
+                sb.AppendLine($"System.String: {item}");
+            }
+            return sb.ToString().TrimEnd();
+        }
+    }
+}
