8ce1b102d8b101a2af8a951e95671e0dc4ea972c
Kalsito1017 <kurabiika34@abv.bg>
2022-10-14 23:05:04 +0300

    Added Exercises

---
+++ !Exams/01.Masterchef/Program.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _01.Masterchef
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] ingredients = Console.ReadLine().Split(' ', StringSplitOptions.RemoveEmptyEntries).Select(int.Parse).ToArray();
+            int[] freshnesslevel = Console.ReadLine().Split(' ', StringSplitOptions.RemoveEmptyEntries).Select(int.Parse).ToArray();
+           SortedDictionary<string, int[]> products = new SortedDictionary<string, int[]>
+            {
+                {"Dipping sauce", new []{150, 0} },
+                {"Green salad", new []{250, 0} },
+                {"Chocolate cake", new []{300, 0} },
+                {"Lobster", new []{400, 0} }
+            };
+            Stack<int> stack = new Stack<int>(freshnesslevel);
+            Queue<int> queue = new Queue<int>(ingredients);
+            while (stack.Count > 0 && queue.Count > 0)
+            {
+                int currIngredient = queue.Dequeue();
+                if (currIngredient == 0)
+                {
+                    continue;
+                }
+                int currFreshness = stack.Pop();
+                int result = currIngredient * currFreshness;
+                bool isCoocked = false;
+                foreach (var dish in products)
+                {
+                    if (dish.Value[0] == result)
+                    {
+                        dish.Value[1]++;
+                        
+                        isCoocked = true;
+                       
+                    }
+                }
+                if (!isCoocked)
+                {
+                    currIngredient += 5;
+                    queue.Enqueue(currIngredient);
+                }
+
+            }
+            if (products.Count(d => d.Value[1] > 0) >= 4)
+            {
+                Console.WriteLine($"Applause! The judges are fascinated by your dishes!");
+            }
+            else
+            {
+                Console.WriteLine($"You were voted off. Better luck next year.");
+            }
+            if (queue.Count > 0)
+            {
+                Console.WriteLine($"Ingredients left: {queue.Sum()}");
+            }
+            foreach (var dish in products.Where(d => d.Value[1] > 0))
+            {
+                Console.WriteLine($" # {dish.Key} --> {dish.Value[1]}");
+            }
+            
+
+
+
+
+
+
+        }
+    }
+}
---
+++ !Exams/02.Survivor/Program.cs
@@ -0,0 +1,101 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _02.Survivor
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {   
+            int rows = int.Parse(Console.ReadLine());
+            var beach = new char[rows][];
+            for (int row = 0; row < beach.GetLength(0); row++)
+            {
+                char[] inputRow = Console.ReadLine().Split(' ', StringSplitOptions.RemoveEmptyEntries).Select(char.Parse).ToArray();
+                beach[row] = inputRow;
+            }
+            int tokens = 0;
+            int opponenttokens = 0;
+            string command = Console.ReadLine();
+            while (command != "Gong")
+            {
+                string[] currArgs = command.Split(' ', StringSplitOptions.RemoveEmptyEntries);
+                string comand = currArgs[0];
+                int row = int.Parse(currArgs[1]);
+                int col = int.Parse(currArgs[2]);
+                if (comand == "Find")
+                {
+                    if (IsIndexValid(beach, row, col))
+                    {
+                        CheckForTokens(beach, row, col, ref tokens);
+                    }
+                }
+                else if (comand == "Opponent")
+                {
+                    string direction = currArgs[3];
+                    if (IsIndexValid(beach, row, col))
+                    {
+                        CheckForTokens(beach, row, col, ref opponenttokens);
+                    }
+                    else
+                    {
+                        continue;
+                    }
+                    for (int steps = 0; steps < 3; steps++)
+                    {
+                        switch(direction)
+                        {
+                            case "up":
+                                row--;
+                                break;
+                            case "down":
+                                row++;
+                                break;
+                            case "left":
+                                col--;
+                                break;
+                            case "right":
+                                col++;
+                                break;
+                        }
+                        if (IsIndexValid(beach, row, col))
+                        {
+                            CheckForTokens(beach, row, col, ref opponenttokens);
+                        }
+                        else
+                        {
+                            break;
+                        }
+                    }
+                    
+                }
+                
+            }
+
+            PrintBeach(beach);
+            Console.WriteLine($"Collected tokens: {tokens}");
+            Console.WriteLine($"Opponent's tokens: {opponenttokens}");
+
+        }
+        private static bool IsIndexValid(char[][] beach, int row, int col)
+        {
+            return row >= 0 && col >= 0 && row < beach.GetLength(0) && col < beach[row].Length;
+        }
+        private static void CheckForTokens(char[][] beach, int row, int col, ref int tokens)
+        {
+            if (beach[row][col] == 'T')
+            {
+                tokens++;
+                beach[row][col] = '-';
+            }
+        }
+        private static void PrintBeach(char[][] beach)
+        {
+            for (int row = 0; row < beach.GetLength(0); row++)
+            {
+                Console.WriteLine(String.Join(" ", beach[row]));
+            }
+        }
+    }
+}
