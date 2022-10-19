0bb8d85ef9e0c0bcc63fb06e420168e9dc4f7513
Kalsito1017 <kurabiika34@abv.bg>
2022-10-19 18:46:22 +0300

    Added 2 Exam Exercises

---
+++ !Exams/01. Lootbox/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _01._Lootbox
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] firstlootbox = Console.ReadLine().Split(" ").Select(int.Parse).ToArray();
+            int[] secondlootbox = Console.ReadLine().Split(" ").Select(int.Parse).ToArray();
+            Queue<int> firstbox = new Queue<int>(firstlootbox); 
+            Stack<int> secondbox = new Stack<int>(secondlootbox);
+            int sum = 0;
+            while (firstbox.Count > 0 && secondbox.Count > 0)
+            {
+                int firstitem = firstbox.Peek();
+                int seconditem = secondbox.Pop();
+                int currsum = firstitem + seconditem;
+                if (currsum % 2 == 0)
+                {
+                    
+                    sum += currsum;
+                    firstbox.Dequeue();
+                    
+                }
+                else
+                {
+                    firstbox.Enqueue(seconditem);
+                }
+            }
+            if (firstbox.Count == 0)
+            {
+                Console.WriteLine("First lootbox is empty");
+            }
+            if (secondbox.Count == 0)
+            {
+                Console.WriteLine("Second lootbox is empty");
+            }
+            if (sum >= 100)
+            {
+                Console.WriteLine($"Your loot was epic! Value: {sum}");
+            }
+            else
+            {
+                Console.WriteLine($"Your loot was poor... Value: {sum}");
+            }
+           
+        }
+    }
+}
---
+++ !Exams/02. Re-Volt/Program.cs
@@ -0,0 +1,215 @@
+﻿using System;
+
+namespace _02._Re_Volt
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+           
+            int n = int.Parse(Console.ReadLine());
+            int numberofcommands = int.Parse(Console.ReadLine());
+            char[,] matrix = new char[n, n];
+            int rowf = 0;
+            int colf = 0;
+            for (int row = 0; row < n; row++)
+            {
+                char[] input = Console.ReadLine().ToCharArray();
+                for (int col = 0; col < n; col++)
+                {
+                    matrix[row,col] = input[col];
+                    if (matrix[row,col] == 'f')
+                    {
+                        rowf = row;
+                        colf = col;
+                    }
+                }
+            }
+            bool hasWIn = false;
+            for (int i = 0; i < numberofcommands; i++)
+            {
+                string commands = Console.ReadLine();
+                matrix[rowf, colf] = '-';
+                
+                switch (commands)
+                {
+                    case "up": rowf--;
+                        Teleport(IsInside(matrix, rowf, colf), ref rowf, ref colf, matrix);
+                        if (matrix[rowf, colf] == 'F')
+                        {
+                            matrix[rowf, colf] = 'f';
+                            hasWIn = true;
+                            break;
+                        }
+                        
+                        else if (matrix[rowf,colf] == 'B')
+                        {
+                            rowf--;
+                            Teleport(IsInside(matrix, rowf, colf), ref rowf, ref colf, matrix);
+                            if (matrix[rowf, colf] == 'F')
+                            {
+                                matrix[rowf, colf] = 'f';
+                                hasWIn = true;
+                                break;
+                            }
+                            matrix[rowf, colf] = 'f';
+
+                        }
+                        else if (matrix[rowf,colf] == 'T')
+                        {
+                            rowf++;
+                            matrix[rowf, colf] = 'f';
+                        }
+                        else
+                        {
+                            matrix[rowf, colf] = 'f';
+                        }
+                        break;
+                    case "down":
+                        rowf++;
+                        Teleport(IsInside(matrix, rowf, colf), ref rowf, ref colf, matrix);
+                        if (matrix[rowf, colf] == 'F')
+                        {
+                            matrix[rowf, colf] = 'f';
+                            hasWIn = true;
+                            break;
+                        }
+
+                        else if (matrix[rowf, colf] == 'B')
+                        {
+                            rowf++;
+                            Teleport(IsInside(matrix, rowf, colf), ref rowf, ref colf, matrix);
+                            if (matrix[rowf, colf] == 'F')
+                            {
+                                matrix[rowf, colf] = 'f';
+                                hasWIn = true;
+                                break;
+                            }
+                            matrix[rowf, colf] = 'f';
+
+                        }
+                        else if (matrix[rowf, colf] == 'T')
+                        {
+                            rowf++;
+                            matrix[rowf, colf] = 'f';
+                        }
+                        else
+                        {
+                            matrix[rowf, colf] = 'f';
+                        }
+                        break;
+                    case "right":
+                        rowf++;
+                        Teleport(IsInside(matrix, rowf, colf), ref rowf, ref colf, matrix);
+                        if (matrix[rowf, colf] == 'F')
+                        {
+                            matrix[rowf, colf] = 'f';
+                            hasWIn = true;
+                            break;
+                        }
+
+                        else if (matrix[rowf, colf] == 'B')
+                        {
+                            colf++;
+                            Teleport(IsInside(matrix, rowf, colf), ref rowf, ref colf, matrix);
+                            if (matrix[rowf, colf] == 'F')
+                            {
+                                matrix[rowf, colf] = 'f';
+                                hasWIn = true;
+                                break;
+                            }
+                            matrix[rowf, colf] = 'f';
+
+                        }
+                        else if (matrix[rowf, colf] == 'T')
+                        {
+                            rowf++;
+                            matrix[rowf, colf] = 'f';
+                        }
+                        else
+                        {
+                            matrix[rowf, colf] = 'f';
+                        }
+                        break;
+                    case "left":
+                        colf--;
+                        Teleport(IsInside(matrix, rowf, colf), ref rowf, ref colf, matrix);
+                        if (matrix[rowf, colf] == 'F')
+                        {
+                            matrix[rowf, colf] = 'f';
+                            hasWIn = true;
+                            break;
+                        }
+
+                        else if (matrix[rowf, colf] == 'B')
+                        {
+                            colf--;
+                            Teleport(IsInside(matrix, rowf, colf), ref rowf, ref colf, matrix);
+                            if (matrix[rowf, colf] == 'F')
+                            {
+                                matrix[rowf, colf] = 'f';
+                                hasWIn = true;
+                                break;
+                            }
+                            matrix[rowf, colf] = 'f';
+                        }
+                        else if (matrix[rowf, colf] == 'T')
+                        {
+                            rowf++;
+                            matrix[rowf, colf] = 'f';
+                        }
+                        else
+                        {
+                            matrix[rowf, colf] = 'f';
+                        }
+                        break;
+                }
+                if (hasWIn)
+                {
+                    break;
+                }
+            }
+            Console.WriteLine(hasWIn ? "Player won!" : "Player lost!");
+            PrintField(matrix);
+        }
+        private static void Teleport(bool b , ref int rowP, ref int colP, char[,] matrix)
+        {
+            if (!b)
+            {
+                if (rowP< 0)
+                {
+                    rowP = matrix.GetLength(0) - 1;
+                }
+                else if (colP < 0)
+                {
+                    colP = matrix.GetLength(1) - 1;
+                }
+                else if (rowP == matrix.GetLength(0))
+                {
+                    rowP = 0;
+                }
+                else if (colP == matrix.GetLength(1))
+                {
+                    colP = 0;
+                }
+            }
+        }
+        private static bool IsInside(char[,] matrix,int row, int col)
+        {
+            return row >= 0 && row < matrix.GetLength(0) &&
+                   col >= 0 && col < matrix.GetLength(1);
+        }
+        private static void PrintField(char[,] matrix)
+        {
+            for (int row = 0; row < matrix.GetLength(0); row++)
+            {
+                for (int col = 0; col < matrix.GetLength(1); col++)
+                {
+                    Console.Write(matrix[row, col]);
+                }
+
+                Console.WriteLine();
+            }
+        }
+    }
+}
